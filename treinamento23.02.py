737431e0cb82b4eacfa7fed345b63d91803d3485
Biinahc <sabrinacaldasads@gmail.com]>
2026-02-23 20:42:49 -0400

    exercicio

--- treinamento23.02.py
+++ treinamento23.02.py
@@ -171,3 +171,15 @@ def saudacao_procedimento(nome):
 
 saudacao_procedimento("João")
 
+#escreva uma função que retorne o maior de dois numeros valores esperados:
+
+
+def maior(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+    
+print(maior(5, 6))
+print(maior(2, 1))
+print(maior(7, 7))
